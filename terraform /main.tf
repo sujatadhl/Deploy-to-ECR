68e2a38efbd01739f1b1598b54e8b6ed78f6281a
sujatadhl <dahalsujata52@gmail.com>
2024-06-06 17:03:06 +0545

    Added terraform files

---
+++ terraform /main.tf
@@ -0,0 +1,30 @@
+module "ec2" {
+  for_each = var.name
+  source                      = "terraform-aws-modules/ec2-instance/aws"
+  version                     = "5.5.0"
+  instance_type               = var.instance_type
+  ami                         = var.ami_id
+  associate_public_ip_address = var.associate_public_ip_address
+  vpc_security_group_ids      = var.sg_id
+  subnet_id                   = var.subnet_id 
+  create_iam_instance_profile = var.create_iam_instance_profile
+  iam_role_description = "SSM Role for accessing EC2 instance"
+  iam_role_policies = {
+    SSM = var.ssm_policy
+    EC2 = var.ec2_ecr_policy
+  }
+  key_name = try(var.key_name, null)
+  tags = {
+    Name = each.value
+  }
+    
+}
+
+resource "aws_ecr_repository" "sujata_ecr" {
+  name                 = var.name
+  image_tag_mutability = var.image_tag_mutability
+
+  image_scanning_configuration {
+    scan_on_push = var.scan_on_push
+  }
+}
---
+++ terraform /variables.tf
@@ -0,0 +1,38 @@
+variable "region" {
+  description = "Region be used for all the resources"
+  type        = string
+}
+
+variable "project" {
+  description = "Name to be used on all the resources as identifier"
+  type        = string
+}
+
+variable "terraform" {
+  description = "Name to be used on all the resources as identifier"
+  type        = bool
+  default     = true
+}
+
+variable "owner" {
+  description = "Name to be used on all the resources as identifier"
+  type        = string
+}
+
+variable "silo" {
+  description = "Name to be used on all the resources as identifier"
+  type        = string
+}
+
+variable "ami_id" {}
+variable "instance_type" {}
+variable "sg_id" {}
+variable "subnet_id" {}
+variable "associate_public_ip_address" {}
+variable "create_iam_instance_profile" {}
+variable "ssm_policy" {}
+variable "ec2_ecr_policy" {}
+variable "key_name" {}
+variable "name" {}
+variable "image_tag_mutability" {}
+variable "scan_on_push" {}
