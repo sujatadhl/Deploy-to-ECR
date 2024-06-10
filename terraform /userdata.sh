468a62b9dc051902e1a70803ee184f3f28b6dcf9
sujatadhl <dahalsujata52@gmail.com>
2024-06-10 11:07:43 +0545

    Added workflow file

--- terraform /userdata.sh
+++ terraform /userdata.sh
@@ -9,7 +9,7 @@ sudo apt-get install -y docker.io
 sudo apt-get install -y awscli
 
 # Log in to ECR
-$(aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 426857564226.dkr.ecr.us-west-2.amazonaws.com)
+aws ecr get-login-password --region us-east-1| docker login --username AWS --password-stdin 426857564226.dkr.ecr.us-east-1.amazonaws.com
 
 # Copr files from s3 to EC2
 aws s3 cp s3://sujata-docker/. /home/ubuntu  --recursive
@@ -22,20 +22,20 @@ cd react/
 docker build -t front-end:latest .
 
 # Tag the Docker image
-docker tag front-end:latest 426857564226.dkr.ecr.us-west-2.amazonaws.com/sujata-ecr
+docker tag front-end:latest 426857564226.dkr.ecr.us-east-1.amazonaws.com/sujata-ecr
 
 # Push the Docker image to ECR
-docker push 426857564226.dkr.ecr.us-west-2.amazonaws.com/sujata-ecr
+docker push 426857564226.dkr.ecr.us-east-1.amazonaws.com/sujata-ecr
 
 # Build the Docker image for backend
 cd /home/ubuntu/react-app/node
 docker build -t back-end:latest .
 
 # Tag the Docker image
-docker tag back-end:latest 426857564226.dkr.ecr.us-west-2.amazonaws.com/sujata-ecr
+docker tag back-end:latest 426857564226.dkr.ecr.us-east-1.amazonaws.com/sujata-ecr
 
 # Push the Docker image to ECR
-docker push 426857564226.dkr.ecr.us-west-2.amazonaws.com/sujata-ecr
+docker push 426857564226.dkr.ecr.us-east-1.amazonaws.com/sujata-ecr
 
 # Install Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
